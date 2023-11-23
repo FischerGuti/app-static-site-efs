63bcc29cdda7879902373c0accfd533c40baf003
FischerGuti <vfischer634@gmail.com>
2023-11-22 21:52:02 -0300

    aaaai

--- terraform/scripts/user_data.sh
+++ terraform/scripts/user_data.sh
@@ -5,21 +5,21 @@ yum install -y amazon-efs-utils httpd telnet tree git
 #wget https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py
 #/usr/bin/python3 /tmp/get-pip.py
 #/usr/local/bin/pip3 install botocore
-mkdir /mnt/efs
+mkdir /mnt
 #echo "${efs_id}:/ /mnt/efs efs _netdev,noresvport,tls 0 0" >> /etc/fstab
 x=10
 while (( $x > 0 )); do
   mount -fav
-  mnt=`df -h |grep /mnt/efs |wc -l`
+  mnt=`df -h |grep /mnt/ |wc -l`
   if (( $mnt >= 1 )); then
     systemctl enable httpd
     cd /tmp
     git clone https://github.com/kledsonhugo/app-static-site-efs
-    mkdir /mnt/efs/html
-    cp /tmp/app-static-site-efs/app/*.html /mnt/efs/html
-    cp /tmp/app-static-site-efs/app/phpinfo.php /mnt/efs/html
+    mkdir /mnt//html
+    cp /tmp/app-static-site-efs/app/*.html /mnt/html
+    cp /tmp/app-static-site-efs/app/phpinfo.php /mnt/html
     rm -rf /var/www/html/
-    ln -s /mnt/efs/html/ /var/www/html
+    ln -s /mnt/html/ /var/www/html
     service httpd restart
     break
   fi
